f2b9386e491bbeefca0ad5db7359db5f63aa3b49
Adarshpal Brar <adarshpal.brar@fdj-gs.com>
2020-11-21 17:13:30 +0000

    Added DOB

--- src/main/java/com/springwell/service/UsersService.java
+++ src/main/java/com/springwell/service/UsersService.java
@@ -3,6 +3,8 @@ package com.springwell.service;
 import com.springwell.model.Users;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +19,7 @@ public class UsersService {
         users.setEmail("adarshpal.brar@gmail.com");
         users.setJobDescription("Java Developer");
         users.setAge(40);
+        users.setDateOfBirth(LocalDate.of(1980, Month.DECEMBER, 7));
         List<Users> usersList = Arrays.asList(users);
         return usersList;
     }
